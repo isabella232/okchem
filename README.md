2ca66f35d38d85991ccfbf1e6d2f40d38629eed8
Christopher Groskopf <staringmonkey@gmail.com>
2014-07-18 14:09:36 -0700

    Fix importer.

---
+++ README.md
@@ -0,0 +1,22 @@
+okchem
+======
+
+Notes: https://docs.google.com/document/d/1iyCDyMfhj6Awf4a7ERWDanzc2z0eh4-mSPW__iOZrTY/edit
+
+Tier2
+-----
+
+Unzip the `.mer` data files into `tier2/data`.
+
+Import the data into psql:
+
+```
+cd tier2
+./process.sh
+```
+
+RMP
+---
+
+TKTK
+
--- tier2/process.sh
+++ tier2/process.sh
@@ -12,13 +12,14 @@ createdb tier2
 cat create.sql | psql -q tier2
 
 # Loop over original files
-for filename in *.mer
+for path in data/*.mer
 do
+    filename=$(basename "$path")
+    extension="${filename##*.}"
     slug="${filename%.*}"
 
-    ./clean.py $slug.mer temp/$slug.csv
+    ./clean.py $path temp/$slug.csv
     in2csv -e latin1 temp/$slug.csv > temp/$slug.utf8.csv
-    #csvstat $slug.utf8.csv > $slug.stats
 
     psql -q tier2 -c "COPY $slug FROM '`pwd`/temp/$slug.utf8.csv' DELIMITER ',' CSV;"
 done

eae0e6f43114f0f62f07893dc1b08980f89f5392
Christopher Groskopf <staringmonkey@gmail.com>
2014-07-18 15:03:12 -0700

    Working on rmp data.

--- README.md
+++ README.md
@@ -3,6 +3,14 @@ okchem
 
 Notes: https://docs.google.com/document/d/1iyCDyMfhj6Awf4a7ERWDanzc2z0eh4-mSPW__iOZrTY/edit
 
+Setup
+-----
+
+```
+pip install requirements.txt
+brew install mdbtools
+```
+
 Tier2
 -----
 
@@ -12,11 +20,17 @@ Import the data into psql:
 
 ```
 cd tier2
-./process.sh
+./import.sh
 ```
 
 RMP
 ---
 
-TKTK
+Unzip the RMP data and move the `*.mdb` files into `rmp/data`.
 
+Import the data into psql:
+
+```
+cd rmp
+./import.sh
+```
---
+++ rmp/import.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# Cleanup from previous runs
+dropdb rmp
+
+# Get psql ready
+createdb rmp 
+
+# Generate database schema
+mdb-schema data/RMPData.mdb postgres > schema.sql
+
+# mdbtools generates bool columns but integer values :(
+sed -i '' "s/BOOL\ NOT\ NULL/INTEGER/" schema.sql
+
+cat schema.sql | psql -q rmp
+rm schema.sql
+
+# Import order so FKs don't error
+declare -a tables=(
+    "tlkpAdminRefreshInfo"
+    "tlkpChemicals"
+    "tlkpCountyFIPSCodes"
+    "tlkpDeregistrationReason"
+    "tlkpDocType"
+    "tlkpForeignCountry"
+    "tlkpLatLongDescriptions"
+    "tlkpLatLongMethods"
+    "tlkpNAICS"
+    "tlkpPhysicalStateCodes"
+    "tlkpStateFIPSCodes"
+    "tlkpTopographyCode"
+    "tlkpWindSpeedUnitCodes"
+    "tlkpDocHandle"
+    "tlkpRejectReason"
+    "tlkpSubmissionReasonCodes"
+
+    "tblS1Facilities"
+    # "tblS1Facilities_ChangeHistory"
+    "tblS1Processes"
+    # "tblS1Processes_ChangeHistory"
+    "tblS1Process_NAICS"
+    # "tblS1Process_NAICS_ChangeHistory"
+    "tblS1ProcessChemicals"
+    # "tblS1ProcessChemicals_ChangeHistory"
+    "tblS1FlammableMixtureChemicals"
+
+    "tlkpS2ScenarioCodes"
+    "tblS2ToxicsWorstCase"
+    # "tblS2ToxicsWorstCase_ChangeHistory"
+    
+    "tblS3ToxicsAltReleases"
+    # "tblS3ToxicsAltReleases_ChangeHistory"
+    
+    "tblS4FlammablesWorstCase"
+    # "tblS4FlammablesWorstCase_ChangeHistory"
+    
+    "tblS5FlammablesAltReleases"
+    # "tblS5FlammablesAltReleases_ChangeHistory"
+    
+    "tlkpS6InitiatingEvents"
+    "tblS6AccidentChemicals"
+    # "tblS6AccidentChemicals_ChangeHistory"
+    "tblS6AccidentHistory"
+    # "tblS6AccidentHistory_ChangeHistory"
+    "tblS6FlammableMixtureChemicals"
+    # "tblS6FlammableMixtureChemicals_ChangeHistory"
+    
+    "tblS7_Prevention_Program_Chemicals"
+    # "tblS7_Prevention_Program_Chemicals_ChangeHistory"
+    "tblS7PreventionProgram3"
+    # "tblS7PreventionProgram3_ChangeHistory"
+    # "tblS7PreventionProgram3Description_ChangeHistory"
+    
+    "tblS8PreventionProgram2"
+    # "tblS8PreventionProgram2_ChangeHistory"
+    "tblS8_Prevention_Program_Chemicals"
+    # "tblS8_Prevention_Program_Chemicals_ChangeHistory"
+    # "tblS8PreventionProgram2Description_ChangeHistory"
+    
+    "tblS9EmergencyResponses"
+    # "tblS9EmergencyResponses_ChangeHistory"
+    
+    "tblExecutiveSummaries"
+    # "tblExecutiveSummaries_ChangeHistory"
+
+    "tblRMPError"
+    "tblRMPTrack"
+)
+
+for table in "${tables[@]}"; do
+    echo "Importing $table"
+    mdb-export -I postgres -q \' data/RMPData.mdb "$table" | psql -q rmp
+done

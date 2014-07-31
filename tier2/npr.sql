dfc4240854eccac366e0c991a039f41cb5c46fd7
Christopher Groskopf <staringmonkey@gmail.com>
2014-07-31 15:41:40 -0500

    Only stash IDs if not empty.

--- tier2/npr.sql
+++ tier2/npr.sql
@@ -1,10 +1,10 @@
 SELECT AddGeometryColumn ('public', 'tier2facilities', 'geom', 4269, 'POINT', 2);
 
 ALTER TABLE Tier2Facilities ADD COLUMN duns_id varchar;
-UPDATE Tier2Facilities SET duns_id = id FROM Tier2FacilityIds WHERE Tier2FacilityIDs.FacilityRecordID = Tier2Facilities.FacilityRecordID and type = 'Dun & Bradstreet';
+UPDATE Tier2Facilities SET duns_id = id FROM Tier2FacilityIds WHERE Tier2FacilityIDs.FacilityRecordID = Tier2Facilities.FacilityRecordID and type = 'Dun & Bradstreet' and Id <> '';
 
 ALTER TABLE Tier2Facilities ADD COLUMN rmp_id varchar;
-UPDATE Tier2Facilities SET rmp_id = id FROM Tier2FacilityIds WHERE Tier2FacilityIDs.FacilityRecordID = Tier2Facilities.FacilityRecordID and type = 'RMP';
+UPDATE Tier2Facilities SET rmp_id = id FROM Tier2FacilityIds WHERE Tier2FacilityIDs.FacilityRecordID = Tier2Facilities.FacilityRecordID and type = 'RMP' and Id <> '';
 
 ALTER TABLE Tier2Facilities ADD COLUMN naics_code varchar;
-UPDATE Tier2Facilities SET naics_code = id FROM Tier2FacilityIds WHERE Tier2FacilityIDs.FacilityRecordID = Tier2Facilities.FacilityRecordID and type = 'NAICS';
+UPDATE Tier2Facilities SET naics_code = id FROM Tier2FacilityIds WHERE Tier2FacilityIDs.FacilityRecordID = Tier2Facilities.FacilityRecordID and type = 'NAICS' and Id <> '';

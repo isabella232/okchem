89debc58da1917ab28d90c929a2d4e639fa2309a
Tyler Fisher <tyler@tylerjfisher.com>
2014-08-01 11:15:39 -0400

    largest facilities by chemical amount

---
+++ tier2/largest_facilities.sql
@@ -0,0 +1,7 @@
+SELECT Tier2Facilities.FacilityName, Tier2Facilities.FacilityRecordID, sum(Tier2ChemicalsInInventory.aveamount_from_code) as sum_AveAmount
+FROM Tier2Facilities
+JOIN Tier2ChemicalsInInventory
+    ON Tier2ChemicalsInInventory.CFacilityRouteRecordID = Tier2Facilities.FacilityRecordID
+WHERE Tier2ChemicalsInInventory.aveamount_from_code IS NOT NULL
+GROUP BY Tier2Facilities.FacilityName, Tier2Facilities.FacilityRecordID
+ORDER BY sum_AveAmount DESC;
--- tier2/npr.sql
+++ tier2/npr.sql
@@ -9,7 +9,7 @@ UPDATE Tier2Facilities SET rmp_id = id FROM Tier2FacilityIds WHERE Tier2Facility
 ALTER TABLE Tier2Facilities ADD COLUMN naics_code varchar;
 UPDATE Tier2Facilities SET naics_code = id FROM Tier2FacilityIds WHERE Tier2FacilityIDs.FacilityRecordID = Tier2Facilities.FacilityRecordID and type = 'NAICS' and Id <> '';
 
-CREATE VIEW no_oil AS SELECT * FROM tier2facilities WHERE "naics_code" NOT LIKE '21%' AND "naics_code" NOT LIKE '22%' AND "naics_code" != '454312' AND "naics_code"NOT LIKE '48611%'
+CREATE VIEW no_oil AS SELECT * FROM tier2facilities WHERE "naics_code" NOT LIKE '21%' AND "naics_code" NOT LIKE '22%' AND "naics_code" != '454312' AND "naics_code"NOT LIKE '48611%';
 
 ALTER TABLE Tier2ChemicalsInInventory ADD COLUMN aveamount_from_code integer;
 UPDATE Tier2ChemicalsInInventory SET aveamount_from_code =

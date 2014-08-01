7923e8b694dd873df49b42ca2320d42d8d51a37d
Tyler Fisher <tyler@tylerjfisher.com>
2014-08-01 11:18:13 -0400

    limit to 20

--- tier2/largest_facilities.sql
+++ tier2/largest_facilities.sql
@@ -4,4 +4,5 @@ JOIN Tier2ChemicalsInInventory
     ON Tier2ChemicalsInInventory.CFacilityRouteRecordID = Tier2Facilities.FacilityRecordID
 WHERE Tier2ChemicalsInInventory.aveamount_from_code IS NOT NULL
 GROUP BY Tier2Facilities.FacilityName, Tier2Facilities.FacilityRecordID
-ORDER BY sum_AveAmount DESC;
+ORDER BY sum_AveAmount DESC
+LIMIT 20;

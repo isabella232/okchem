de8774505f6824a335ce443835754d9ae14c8108
Christopher Groskopf <staringmonkey@gmail.com>
2014-08-01 09:36:07 -0500

    Add aveamount_from_code column to tier2.

--- tier2/npr.sql
+++ tier2/npr.sql
@@ -8,3 +8,21 @@ UPDATE Tier2Facilities SET rmp_id = id FROM Tier2FacilityIds WHERE Tier2Facility
 
 ALTER TABLE Tier2Facilities ADD COLUMN naics_code varchar;
 UPDATE Tier2Facilities SET naics_code = id FROM Tier2FacilityIds WHERE Tier2FacilityIDs.FacilityRecordID = Tier2Facilities.FacilityRecordID and type = 'NAICS' and Id <> '';
+
+ALTER TABLE Tier2ChemicalsInInventory ADD COLUMN aveamount_from_code integer;
+UPDATE Tier2ChemicalsInInventory SET aveamount_from_code = 
+    CASE
+        WHEN aveamountcode::integer = 1 THEN 50
+        WHEN aveamountcode::integer = 2 THEN 250
+        WHEN aveamountcode::integer = 3 THEN 750
+        WHEN aveamountcode::integer = 4 THEN 2500
+        WHEN aveamountcode::integer = 5 THEN 7500
+        WHEN aveamountcode::integer = 6 THEN 17500
+        WHEN aveamountcode::integer = 7 THEN 37500
+        WHEN aveamountcode::integer = 8 THEN 62500
+        WHEN aveamountcode::integer = 9 THEN 87500
+        WHEN aveamountcode::integer = 10 THEN 300000
+        WHEN aveamountcode::integer = 11 THEN 750000
+        WHEN aveamountcode::integer = 12 THEN 5500000
+        WHEN aveamountcode::integer = 13 THEN 10000000
+    END;
